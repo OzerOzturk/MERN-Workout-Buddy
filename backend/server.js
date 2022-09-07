ab9da7f9b19f06f935fe6341a0c0147edfed90d0
OzerOzturk <ozerozturk1636@gmail.com>
2022-09-07 23:51:17 +0300

    connect to the database.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 //express app
@@ -17,7 +18,18 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/workouts", workoutRoutes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("hey listening the port I created on", process.env.PORT);
-});
+//connect to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(
+        "hey connected to database and listening the port I created on",
+        process.env.PORT
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
